28c875a375f4fe650c0dcecee4202db729c3d0c3
Toni Rämö <toni.ramo@helsinki.fi>
2022-10-13 16:59:32 +0300

    palauta t5.7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,37 @@
-import { React } from 'react';
+import { React, useState } from 'react';
 
 const Blog = ({ blog }) => {
+  const [fullView, setFullView] = useState(false);
+
+  const toggleFullView = () => {
+    setFullView(!fullView);
+  };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  };
+  if (fullView) {
+    return (
+      <div style={blogStyle}>
+        <div>
+          <b>{blog.title}</b>
+          <button onClick={toggleFullView}>Hide</button>
+        </div>
+        <div>url: {blog.url}</div>
+        <div>likes: {blog.likes}</div>
+        <div>author: {blog.author}</div>
+      </div>
+    );
+  }
   return(
-    <div>{`${blog.title} ${blog.author}`}</div>
+    <div style={blogStyle}>
+      {`${blog.title} by ${blog.author}`}
+      <button onClick={toggleFullView}>View</button>
+    </div>
   );
 };
 export default Blog;
