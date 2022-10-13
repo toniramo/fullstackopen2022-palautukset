54a61fbd36c4fb4eb381b548e302b5660c275bff
Toni Rämö <toni.ramo@helsinki.fi>
2022-10-13 15:04:37 +0300

    päivitä eslint vastaamaan tehtävänantoa

--- osa5/bloglist-frontend/.eslintrc.js
+++ osa5/bloglist-frontend/.eslintrc.js
@@ -1,25 +1,55 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
+  "env": {
+    "browser": true,
+    "es6": true,
+    "jest/globals": true
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    "ecmaVersion": 2021,
+    "sourceType": "module"
   },
-  plugins: ['react', 'prettier'],
-  rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        singleQuote: true,
-        parser: 'flow',
-      },
+  "plugins": [
+    "react", "jest"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+      2
     ],
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "eqeqeq": "error",
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": [
+      "error", "always"
+    ],
+    "arrow-spacing": [
+      "error", { "before": true, "after": true }
+    ],
+    "no-console": 0,
+    "react/prop-types": 0,
+    "react/react-in-jsx-scope": "off"
   },
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  }
 };
