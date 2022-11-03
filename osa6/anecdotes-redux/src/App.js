664140ccaa42be5a497b5b57741fd077ba63dd7f
Toni Rämö <toni.ramo@helsinki.fi>
2022-11-03 15:09:56 +0200

    palauta t6.8

--- osa6/anecdotes-redux/src/App.js
+++ osa6/anecdotes-redux/src/App.js
@@ -1,29 +1,12 @@
-import { useSelector, useDispatch } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
-import { vote } from './reducers/anecdoteReducer'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const voteAnecdote = (id) => {
-    dispatch(vote(id))
-  }
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => voteAnecdote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   )
