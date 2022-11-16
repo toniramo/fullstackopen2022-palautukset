b02f3c5c1765de5bfaec7eba685fcf2adc217fb5
Toni Rämö <toni.ramo@helsinki.fi>
2022-11-16 15:38:39 +0200

    poista kommentoidut rivit

--- osa7/bloglist-frontend/src/components/Blog.js
+++ osa7/bloglist-frontend/src/components/Blog.js
@@ -1,20 +1,7 @@
-import { React, /* useState */ } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 import { Link } from 'react-router-dom';
-// import { likeBlog, removeBlog } from '../reducers/blog';
 
 const Blog = ({ blog }) => {
-  // const dispatch = useDispatch();
-
-  // const user = useSelector(state => state.user);
-
-  //  const [fullView, setFullView] = useState(false);
-
-  /*
-  const toggleFullView = () => {
-    setFullView(!fullView);
-  };
-  */
 
   const blogStyle = {
     paddingTop: 10,
@@ -25,58 +12,15 @@ const Blog = ({ blog }) => {
     padding: 5,
   };
 
-  /*
-  const removeButtonStyle = {
-    background: 'salmon',
-    border: 'solid',
-    borderWidth: 0.1,
-    display: blog.user.username === user.username ? '' : 'none',
-  };
-
-  const handleLike = () => {
-    dispatch(likeBlog(blog));
-  };
-
-  const handleRemove = () => {
-    if (window.confirm(`Remove ${blog.title} by ${blog.author}?`)) {
-      dispatch(removeBlog(blog));
-    }
-  };
-  */
-
   const header = (
     <>
       <b>{blog.title}</b> by {blog.author}
     </>
   );
 
-  /*
-  if (fullView) {
-    return (
-      <div style={blogStyle} data-testid="blog-all-info">
-        <div>
-          {header}
-          { <button onClick={toggleFullView}>Hide</button> }
-        </div>
-        <div>url: {blog.url}</div>
-        <div>
-          likes: {blog.likes}
-          <button onClick={handleLike}>Like</button>
-        </div>
-        <div>added by: {blog.user ? blog.user.name : null}</div>
-        <div>
-          <button style={removeButtonStyle} onClick={handleRemove}>
-            Remove
-          </button>
-        </div>
-      </div>
-    );
-  }
-  */
   return (
     <div style={blogStyle} data-testid="blog-header-only">
       <Link to={`/blogs/${blog.id}`}>{header}</Link>
-      {/* <button onClick={toggleFullView}>View</button> */}
     </div>
   );
 };
